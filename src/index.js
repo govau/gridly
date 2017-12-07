77374c8ee3e43006ba9a983973385405855a7c06
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2017-12-07 16:48:06 +1100

    tweaked css and removed replace
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ if( existing.length > 0 ) {
 }
 else {
 	var overlay = document.createElement("div");
+	var height = document.getElementsByTagName('body')[0].clientHeight + 100;
 
 	if( overlay.classList ) {
 		overlay.classList.add('js-uikit-gridly');
@@ -13,17 +14,17 @@ else {
 		overlay.className += ' js-uikit-gridly';
 	}
 
-	overlay.style.backgroundImage =    'linear-gradient( #000 2px, transparent 2px ),' +
-		'linear-gradient(90deg, #000 2px, transparent 2px),' +
+	overlay.style.backgroundImage =    'linear-gradient( rgba(50, 50, 50, 0.8) 2px, transparent 2px ),' +
+		'linear-gradient(90deg, rgba(50, 50, 50, 0.8) 2px, transparent 2px),' +
 		'linear-gradient( rgba(0, 0, 0, 0.3) 1px, transparent 1px ),' +
 		'linear-gradient( 90deg, rgba(0, 0, 0, 0.3) 1px, transparent 1px )';
 	overlay.style.backgroundSize =     '64px 64px, 64px 64px, 8px 8px, 8px 8px';
 	overlay.style.backgroundPosition = '-2px -2px, -2px -2px, -1px -1px, -1px -1px';
-	overlay.style.position = 'fixed';
+	overlay.style.position = 'absolute';
 	overlay.style.top = '0';
 	overlay.style.right = '0';
-	overlay.style.bottom = '0';
 	overlay.style.left = '0';
+	overlay.style.height = height + 'px';
 
 	document.body.appendChild( overlay )
 }
